9c55041b586d6fefada25f5ed3e1c1bc6c024ed9
Zesty <adi8116315284@gmail.com>
2024-11-25 21:13:57 +0530

    initialization of files

---
+++ profile.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile | Mybook</title>
+    <link rel="stylesheet" href="styles/profile.css">
+</head>
+<body>
+
+    <!-- top bar-->
+    <div id="blue-bar">
+        <div id="mybook">
+            <span id="mybook-text">Mybook</span> &nbsp;&nbsp; <input type="text" id="search_box" placeholder="Search for people">
+
+            <img src="social-images/selfie-1.jpeg" alt="profile-picture" id="profile-picture">
+        </div>
+    </div>
+
+    <!-- cover area-->
+    <div id="cover-area">
+
+        <div id="cover-container">
+            <img src="social-images/mountain.jpg" alt="mountain" id="mountain-img">
+            <img src="social-images/selfie-1.jpeg" alt="profile-picture" id="profile-picture-round"><br>
+            <div id="zesty-kun">Zesty Kun</div>  
+            <br>
+            <div class="menu-buttons">Timeline</div> 
+            <div class="menu-buttons">About</div> 
+            <div class="menu-buttons">Friends</div> 
+            <div class="menu-buttons">Photos</div> 
+            <div class="menu-buttons">Settings</div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mybook | Signup</title>
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+
+<body>
+    <div id="bar">
+        <div id="mybook">Mybook</div> 
+        <div id="signup_button">Signup</div>
+    </div>
+
+    <div id="login_bar">
+
+        Signup to Mybook<br><br>
+
+        <input type="text" class="text" placeholder="First Name"><br><br>
+        <input type="text" class="text" placeholder="Last Name"><br><br>
+
+        <div id="gender">Gender:</div><br>
+        <select class="text">
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+        </select>
+        <br><br>
+        <input type="text" class="text" placeholder="Email"><br><br>
+        <input type="password" class="text" placeholder="Password"><br><br>
+        <input type="password" class="text" placeholder="Retype Password"><br><br>
+        <input type="submit" id="button" value="Sign up">
+    </div>
+</body>
+</html>
