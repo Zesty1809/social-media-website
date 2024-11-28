d3f4920cfdfb186a58ddbcfae58a6ab4de9d8123
Zesty <adi8116315284@gmail.com>
2024-11-29 00:23:29 +0530

    Adding timeline.

--- timeline.php
+++ timeline.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profile | Mybook</title>
-    <link rel="stylesheet" href="styles/profile.css">
+    <title>Timeline | Mybook</title>
+    <link rel="stylesheet" href="styles/timeline.css">
 </head>
 <body>
 
@@ -20,49 +20,13 @@
     <!-- cover area-->
     <div id="cover-area">
 
-        <div id="cover-container">
-            <img src="social-images/mountain.jpg" alt="mountain" id="mountain-img">
-            <img src="social-images/selfie-1.jpeg" alt="profile-picture" id="profile-picture-round"><br>
-            <div id="zesty-kun">Zesty Kun</div>  
-            <br>
-            <div class="menu-buttons">Timeline</div> 
-            <div class="menu-buttons">About</div> 
-            <div class="menu-buttons">Friends</div> 
-            <div class="menu-buttons">Photos</div> 
-            <div class="menu-buttons">Settings</div>
-        </div>
-
     <!-- below cover area-->
         <div id="flexbox">
             <!-- friends area-->
             <div id="box1">
                 <div id="friends_bar">
-                    Friends <br>
-
-                    <div class="friends">
-                        <img src="social-images/user1_v1.jpg" alt="user1" class="friends_img">
-                        <br>
-                        Daemon Rivera
-                    </div>
-
-                    <div class="friends">
-                        <img src="social-images/user2.jpg" alt="user1" class="friends_img">
-                        <br>
-                        Elgin
-                    </div>
-
-                    <div class="friends">
-                        <img src="social-images/user3.jpg" alt="user1" class="friends_img">
-                        <br>
-                        Keana Reeves
-                    </div>
-
-                    <div class="friends">
-                        <img src="social-images/user4.jpg" alt="user1" class="friends_img">
-                        <br>
-                        Fred Flintstone
-                    </div>
-
+            <img src="social-images/selfie-1.jpeg" alt="profile-picture" id="profile-picture-round"><br>
+            Zesty Kun
                 </div>
             </div>
 
